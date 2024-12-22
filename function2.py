91f992163af887f6fc7fcc983ae02b5eedaea86a
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-22 21:09:22 +0500

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,8 @@
+def absloute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+    
+print ("Absloute value of 89:", absloute_value(89))
+print ("Absloute value of -189:", absloute_value(-189))
